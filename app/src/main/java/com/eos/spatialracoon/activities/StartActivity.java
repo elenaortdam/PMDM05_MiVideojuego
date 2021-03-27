f068eefc7fa20bb3c6c1d4124c354f804ef3945b
HelenaOrt <elenaortdam@gmail.com>
2021-03-27 18:17:08 +0100

    feat: hecha la animación touch

--- app/src/main/java/com/eos/spatialracoon/activities/StartActivity.java
+++ app/src/main/java/com/eos/spatialracoon/activities/StartActivity.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,18 +19,29 @@ public class StartActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start);
-		View racoon = findViewById(R.id.mapache_nave);
-		racoon.setOnClickListener(v -> this.startActivity(new Intent(this, GameActivity.class)));
+		ImageView raccoon = findViewById(R.id.mapache_nave);
+		raccoon.setOnClickListener(v -> this.startActivity(new Intent(this, GameActivity.class)));
 		TextView record = findViewById(R.id.record_start);
 		SharedPreferences sharedPreferences = this.getSharedPreferences(getResources().getString(R.string.app_name),
 																		Context.MODE_PRIVATE);
 		int topScore = sharedPreferences.getInt("SCORE", 0);
 		record.setText(String.valueOf(topScore));
+		ImageView touch = findViewById(R.id.touch);
+
+		TranslateAnimation clickAnimation = new TranslateAnimation(0, -80,
+																   0, 80);
+		clickAnimation.setDuration(1500);  // duración de la animación
+		clickAnimation.setRepeatCount(Animation.INFINITE);
+		clickAnimation.setRepeatMode(Animation.REVERSE);
+		touch.startAnimation(clickAnimation);
+
+		TranslateAnimation raccoonAnimation = new TranslateAnimation(0, 0,
+																	 0, 50);
+		raccoonAnimation.setDuration(1000);  // duración de la animación
+		raccoonAnimation.setRepeatCount(Animation.INFINITE);
+		raccoonAnimation.setRepeatMode(Animation.REVERSE);
+		raccoon.startAnimation(raccoonAnimation);
+
 	}
-
-	//TODO: elena animación mapache (?)
-	//TODO: elena record encima del mapache
 	//TODO: elena botón instrucciones
-	//TODO: elena animación click
-
 }
